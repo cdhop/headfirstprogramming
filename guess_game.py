81edd55679a37cb6e6d76caae8fc6de3d59b0a13
cdhop <cdhop@subaru>
2015-12-13 09:24:05 -0700

    Adjusted indents to be PEP8 compliant

--- guess_game.py
+++ guess_game.py
@@ -8,14 +8,14 @@ print("Welcome!")
 guess = 0
 
 while guess != secret:
-	g = input("Guess the number: ")
-	guess = int(g)
-	if guess == secret:
-		print("You win!")
-	else:
-		if guess > secret:
-			print("Too high!")
-		else:
-			print("Too low!")
+    g = input("Guess the number: ")
+    guess = int(g)
+    if guess == secret:
+        print("You win!")
+    else:
+        if guess > secret:
+            print("Too high!")
+        else:
+            print("Too low!")
 print("Game Over!")
 
--- tvn_data.py
+++ tvn_data.py
@@ -3,35 +3,35 @@
 import sqlite3
 
 def find_details(id2find):
-	result = None
-	db = sqlite3.connect("surfersDB.sdb")
+    result = None
+    db = sqlite3.connect("surfersDB.sdb")
 
-	db.row_factory = sqlite3.Row
-	cursor = db.cursor()
-	cursor.execute("select * from surfers")
-	rows = cursor.fetchall()
+    db.row_factory = sqlite3.Row
+    cursor = db.cursor()
+    cursor.execute("select * from surfers")
+    rows = cursor.fetchall()
 
-	for row in rows:
-		if row['id'] == id2find:
-			result = {}
-			result['id']      = str(row['id'])
-			result['name']    = row['name']
-			result['country'] = row['country']
-			result['average'] = str(row['average'])
-			result['board']   = row['board']
-			result['age']     = str(row['age'])
-	cursor.close()
-	return(result)	
-	
+    for row in rows:
+        if row['id'] == id2find:
+            result = {}
+            result['id']      = str(row['id'])
+            result['name']    = row['name']
+            result['country'] = row['country']
+            result['average'] = str(row['average'])
+            result['board']   = row['board']
+            result['age']     = str(row['age'])
+    cursor.close()
+    return(result)    
+    
 lookup_id = int(input("Enter the id of the surfer: "))
 surfer = find_details(lookup_id)
 if surfer:
-	print("ID:      " + surfer['id'])
-	print("Name:    " + surfer['name'])
-	print("Country: " + surfer['country'])
-	print("Average: " + surfer['average'])
-	print("Board:   " + surfer['board'])
-	print("Age:     " + surfer['age'])
+    print("ID:      " + surfer['id'])
+    print("Name:    " + surfer['name'])
+    print("Country: " + surfer['country'])
+    print("Average: " + surfer['average'])
+    print("Board:   " + surfer['board'])
+    print("Age:     " + surfer['age'])
 else:
-	print("Could not find the surfer")
-	
+    print("Could not find the surfer")
+    
