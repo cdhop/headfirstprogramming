879fc51f760c02214bc51d55d9278f48bbca193d
cdhop <cdhop@subaru>
2015-12-13 09:23:41 -0700

    Added tuple_example

---
+++ tuple_example.py
@@ -0,0 +1,14 @@
+#!/usr/bin/python3
+
+dimensions = (200,50)
+
+print(dimensions[0])
+print(dimensions[1])
+
+for dimension in dimensions:
+    print(dimension)
+
+dimensions = (400,100)
+for dimension in dimensions:
+    print(dimension)
+
